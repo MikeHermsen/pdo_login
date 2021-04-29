fc4e23a38d96f906015647489ff4e964234ccb26
MikeHermsen <68249431+MikeHermsen@users.noreply.github.com>
2021-04-29 14:20:40 +0200

    Update checkValid.php

--- checkValid.php
+++ checkValid.php
@@ -1,19 +1,21 @@
 <?php include_once 'includes/cred.php'?>
 
 <?php
+    // Preparing the db
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
+//  Connecting with the SQL Database
 try {
      $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 
-
+// Preparing the MySQL Code
 $stmt = $pdo->prepare('SELECT * FROM credentials WHERE email = :email');
 $stmt->execute(['email' => strtolower($_POST['email'])]);
 $user = $stmt->fetch();
